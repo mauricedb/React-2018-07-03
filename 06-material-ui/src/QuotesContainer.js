fa384d2fd9e6226b1900a67a6ee14e242733e0a8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-07-04 11:06:50 +0200

    Before demo

---
+++ 06-material-ui/src/QuotesContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import QuotesPresentation from './QuotesPresentation';
+const url = 'http://api.icndb.com/jokes/random/10?limitTo=[nerdy]';
+
+class Quotes extends Component {
+  state = {
+    jokes: [],
+    loading: true,
+    error: null
+  };
+
+  componentDidMount() {
+    fetch(url)
+      .then(rsp => {
+        if (rsp.ok) {
+          return rsp.json();
+        } else {
+          return Promise.reject({ message: rsp.statusMessage });
+        }
+      })
+      .then(data => {
+        const jokes = data.value;
+        this.setState({ jokes, loading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err, loading: false });
+      });
+  }
+  render() {
+    // const { jokes } = this.state;
+
+    // return <QuotesPresentation jokes={jokes} />;
+
+    return <QuotesPresentation {...this.state} />;
+  }
+}
+
+export default Quotes;
---
+++ 06-material-ui/src/SingleQuote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingleQuote = ({ joke }) => {
+  return <li>{joke.joke} </li>;
+};
+
+export default SingleQuote;
